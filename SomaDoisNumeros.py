b94422a952074db75132771cff2b25361de8f30e
IgorVianaa <150079795+IgorVianaa@users.noreply.github.com>
2023-11-06 18:28:08 -0300

    Soma de dois Números

---
+++ SomaDoisNumeros.py
@@ -0,0 +1,11 @@
+print('Vamos fazer a soma de 2 números, me diga quais você quer!')
+
+n1 = int(input('Me diga um número: '))
+n2 = int(input('Me dê outro número: '))
+s = n1+n2
+
+#print('A soma de',n1,'+',n2,'é',s,)
+#print('A soma de {} + {} é {}'.format(n1,n2,s))
+
+print(f'A soma de {n1} + {n2} é {s}')
+
