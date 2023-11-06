0546f1d8869fb02aea64f087cbd743e8bed869a5
IgorVianaa <150079795+IgorVianaa@users.noreply.github.com>
2023-11-06 18:05:08 -0300

    Cadastro

---
+++ DadosParaCadastro.py
@@ -0,0 +1,6 @@
+print('Me forneça alguns dados para cadastro')
+nome=input('Nome: ')
+email=input('Email: ')
+telefone=input('Telefone: ')
+
+print(f'Seja bem vindo {nome}, seu e-mail é: {email} e seu telefone é {telefone}')
