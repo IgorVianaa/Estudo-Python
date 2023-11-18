6f32b2d16b806e72bab1db7d077d854a7fc282ad
IgorVianaa <150079795+IgorVianaa@users.noreply.github.com>
2023-11-18 18:51:37 -0300

    Aprovado, Recuperação ou Reprovado

--- MediaAnual.py
+++ MediaAnual.py
@@ -10,3 +10,10 @@ b4 = float(input('Nota do quarto bimestre: '))
 soma = (b1+b2+b3+b4)/4
 
 print(f'A sua média Anual é de {soma}')
+
+if soma >7:
+    print(f'Parabéns {a1}, Você está Aprovado')
+elif soma >=5 <=6:
+    print(f' {a1} Você está de recuperação')
+else:
+    print(f'Infelizmente {a1}, Está Reprovado')
